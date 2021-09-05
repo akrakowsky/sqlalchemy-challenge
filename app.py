bec4c1f29d818f05248573c3bbffe8d96c0c1d98
Ali Krakowsky <alig_10@comcast.net>
2021-09-04 21:32:59 -0700

    update app

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start/<start><br/>"
+        f"/api/v1.0/start/yyyy-mm-dd<br/>"
         f"/api/v1.0/start/<start>/end/<end>"
     )
 
